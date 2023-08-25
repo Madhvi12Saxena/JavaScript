3135913e8383241173e29934ffe83d5c826d5eea
Madhvi Saxena <ssms122005@gmail.com>
2023-08-25 12:33:57 +0000

    static

---
+++ 10_classes_and_oop/staticprop.js
@@ -0,0 +1,26 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const Madhvi = new User("Madhvi")
+// console.log(Madhvi.createId());  => gives error
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+// console.log(iphone.createId());   => gives error
